c7452f1a64787a9143e77eaf5d17613348aa3e81
santinorullan <santinorullan06@gmail.com>
2026-03-28 16:41:07 -0300

    BFS completo y testeado

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -22,8 +22,43 @@ class BreadthFirstSearch:
         reached = {}
         reached[root.state] = True
 
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        # Primer test objetivo
+        if grid.objective_test(root.state):
+            return Solution(root, reached)
 
+        # Crea la frontera con la clase queue (FIFO) y añade root para luego expandirlo
+        frontier = QueueFrontier()
+        frontier.add(root)
+
+        
+        while not frontier.is_empty():
+            # Saca un nodo de la frontera, el primero que entra, sale primero
+            node = frontier.remove()
+
+            # Se expande el nodo con todas las acciones posibles
+            for action in grid.actions(node.state):
+                successor = grid.result(node.state, action)
+
+                # Chequea que la posicion nueva no haya sido explorada antes, para evitar bucles
+                if successor not in reached:
+                    # Crea el nodo hijo
+                    child = Node(
+                        value="",
+                        state=successor,
+                        cost=node.cost + grid.individual_cost(node.state, action),
+                        parent=node,
+                        action=action
+                    )
+
+                    # Se agrega el nodo hijo al diccionario de alcanzados con valor True
+                    reached[successor] = True
+
+                    # Chequea si successor es la posición obj
+                    if grid.objective_test(successor):
+                        return Solution(child, reached)
+
+                    # Añade el hijo a la frontera
+                    frontier.add(child)
+
+        # Si no encontró solución, devuelve NoSolution
         return NoSolution(reached)
